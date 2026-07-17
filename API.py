8f1252a8080be2c9cf4d630a9afd720e275f46d9
OLUDARE MARTINS-ENOCH <153629786+shalomito@users.noreply.github.com>
2026-07-17 22:00:39 +0100

    Update API.py

--- API.py
+++ API.py
@@ -18,13 +18,14 @@ model_loads = joblib.load("my_fraud_detection_model.pkl")
 #Create a Class for Transactions()
 
 class CustomerTransaction(BaseModel):
-    hour_of_the_day: int
+    hour_of_day: int
     is_night_transaction: int
     merchant_category: str
     transaction_amount: float
-    time_since_last_tnx_hrs: int
+    time_since_last_txn_hrs: int
+    is_international: int
     failed_attempts: int
-    pin_recently_changed: int
+    pin_changed_recently: int
     transaction_hour: int
 
 
@@ -39,15 +40,19 @@ def home():
 def predict(customer: CustomerTransaction):
 
     input_data = pd.DataFrame({
-    'hour_of_the_day': [customer.hour_of_the_day],
+    'hour_of_day': [customer.hour_of_day],
     'is_night_transaction': [customer.is_night_transaction],
     'merchant_category': [customer.merchant_category],
     'transaction_amount': [customer.transaction_amount],
-    'time_since_last_tnx_hrs': [customer.time_since_last_tnx_hrs],
+    'time_since_last_txn_hrs': [customer.time_since_last_txn_hrs],
+    'is_international': [customer.is_international],
     'failed_attempts': [customer.failed_attempts],
-    'pin_recently_changed': [customer.pin_recently_changed],
+    'pin_changed_recently': [customer.pin_changed_recently],
     'transaction_hour': [customer.transaction_hour]
 })
+
+    #print(input_data.columns.tolist())
+    #print(model_loads.feature_names_in_)
 
     predictions_true_false = model_loads.predict(input_data)
     prediction_percent_prob = model_loads.predict_proba(input_data)[0, 1]
@@ -56,12 +61,12 @@ def predict(customer: CustomerTransaction):
 
         return{
             'Result' : 'FRAUD',
-            'Probability' : prediction_percent_prob*100
+            'Probability' : f'{prediction_percent_prob:.1%}'
         }
     else:
         return{
             'Result': 'NOT FRAUD',
-            'Probability' : prediction_percent_prob*100
+            'Probability' : f'{prediction_percent_prob:.1%}'
         }
 
 
